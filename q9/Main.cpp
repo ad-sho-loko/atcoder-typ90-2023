b603174f3a375b29afee4000d8d41c397382f500
adsholoko <ad.sho.loko@gmail.com>
2023-11-02 17:37:20 +0000

    q9

--- q9/Main.cpp
+++ q9/Main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -15,6 +16,7 @@ int N;
 vector<Point> X;
 const double PI = 3.14159265358979;
 
+// 指定した2点から偏角を求める
 double getAngle(Point p1, Point p2) {
     auto xd = p1.x - p2.x;
     auto yd = p1.y - p2.y;
@@ -34,6 +36,15 @@ double getAngle(Point p1, Point p2) {
         return 360.0 - acos(xd / d) * 180.0 / PI;
 }
 
+// 指定した2点の偏角からなす角を求める
+double getAngle2(double I1, double I2) {
+    // 2個のなす角のうち小さいほうが180°以下の角を満たす
+    if (abs(I1 - I2) >= 180)
+        return 360.0 - abs(I1 - I2);
+    else
+        return abs(I1 - I2);
+}
+
 void solveSlow() {
     double maxAngle = 0;
     for (int i = 0; i < N; i++) {
@@ -47,16 +58,48 @@ void solveSlow() {
                 double I2 = getAngle(X[i], X[k]);
                 // cout << "I1:" << I1 << ", I2:" << I2 << " -> " << abs(I1 -
                 // I2) << endl;
-
-                // 2個のなす角のうち小さいほうが180°以下の角を満たす
-                if (abs(I1 - I2) >= 180)
-                    maxAngle = max(360.0 - abs(I1 - I2), maxAngle);
-                else
-                    maxAngle = max(abs(I1 - I2), maxAngle);
+                maxAngle = max(getAngle2(I1, I2), maxAngle);
             }
         }
     }
 
+    printf("%.12lf\n", maxAngle);
+}
+
+void solveFast() {
+    double maxAngle = 0;
+    for (int i = 0; i < N; i++) {
+        // Pi になす角を中心とした偏角を求めてソートする
+        vector<double> I(N);
+        for (int j = 0; j < N; j++) {
+            if (i == j)
+                continue;
+
+            I.push_back(getAngle(X[i], X[j]));
+        }
+        sort(I.begin(), I.end());
+
+        // 偏角をソートすればある二点の偏角を定めればもう一点の選ぶべき偏角を二分探索で探索できる
+        // 例えば偏角が30°をなす二点だとすれば、30+180=210°に近傍するような偏角がふさわしい
+        // よって三点目をO(N) -> O(logN) に計算量を削減することができる
+        for (int j = 0; j < N; j++) {
+            double target = I[j] + 180;
+            if (target >= 360)
+                target -= 360;
+
+            int candIndex1 =
+                lower_bound(I.begin(), I.end(), target) - I.begin(); // O(logN)
+            int candIndex2 = candIndex1 == 0 ? 1 : candIndex1 - 1;
+
+            double angle1 = getAngle2(I[j], I[candIndex1]);
+            double angle2 = getAngle2(I[j], I[candIndex2]);
+
+            // cout << "(" << i << "," << j << ")" << endl;
+            // cout << "I1:" << angle1 << ", I2:" << angle2 << endl;
+
+            maxAngle = max({maxAngle, angle1, angle2});
+        }
+    }
     printf("%.12lf\n", maxAngle);
 }
 
@@ -69,6 +112,6 @@ int main() {
         X.push_back(Point{x, y});
     }
 
-    solveSlow();
+    solveFast();
     return 0;
 }
