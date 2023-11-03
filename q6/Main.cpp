bf67bc630ab1f62adc6c14d9e056906c2deca016
adsholoko <ad.sho.loko@gmail.com>
2023-11-03 16:20:08 +0000

    solve q6

--- q6/Main.cpp
+++ q6/Main.cpp
@@ -9,35 +9,39 @@ int main() {
     cin >> N >> K >> S;
 
     // i行目以降でjのアルファベットが登場する初めての添え字を格納する
-    // eg) abca
-    //     a b c d...
-    // [0] 0 1 2 -1
-    // [1] 3 1 2 -1
-    // [2] 3 -1 2 -1
-    // [3] 3 -1 -1 -1
+    // 登場しない場合はありえない添え字（文字列そのものの長さを格納する）
+    // eg) abca (N=4)
+    //        a b c d e f g...
+    // [0(a)] 0 1 2 4
+    // [1(b)] 3 1 2 4
+    // [2(c)] 3 4 2 4
+    // [3(a)] 3 4 4 4
 
-    int nex[N][26];
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 26; j++) {
-            nex[i][j] = -1;
-        }
+    int nex[N + 1][26];
+    for (int i = 0; i < 26; i++) {
+        nex[N][i] = N;
     }
 
     for (int i = N - 1; i >= 0; i--) {
-        for (int j = i; j >= 0; j--) {
-            nex[j][S[i] - 'a'] = i;
+        for (int j = 0; j < 26; j++) {
+            if ((S[i] - 'a') == j) {
+                nex[i][j] = i;
+            } else {
+                nex[i][j] = nex[i + 1][j];
+            }
         }
     }
 
     // 答えのk文字目にはi文字目以降のj番目のアルファベットを採用できるかチェックする
-    int i = 0;
+    int k = 0;
     string answer = "";
-    for (int k = 0; k < K; k++) {
+    for (int i = 0; k < K;) {
         for (int j = 0; j < 26; j++) {
             // 採用する文字列の最小の添え字（N-nex[i][j]）が残りに必要な文字列の長さ（K-k）を満たすかどうか
             if (nex[i][j] != -1 && N - nex[i][j] >= K - k) {
-                answer += S[j];
+                answer += 'a' + j;
                 i = nex[i][j] + 1;
+                k++;
                 break;
             }
         }
