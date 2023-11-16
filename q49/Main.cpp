7960939cdac6b2b85fe3e26fc3e24918ed7ae7cf
adsholoko <ad.sho.loko@gmail.com>
2023-11-16 04:14:55 +0000

    q49

---
+++ q49/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<long long> C(M), L(M), R(M);
+
+    for (int i = 0; i < M; i++) {
+        cin >> C[i] >> L[i] >> R[i];
+    }
+
+    return 0;
+}
