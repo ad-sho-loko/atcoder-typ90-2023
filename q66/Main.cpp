07a32aa7d8be8fd08c82fbb1af4a85fb80aab926
adsholoko <ad.sho.loko@gmail.com>
2023-11-23 14:41:59 +0000

    solve q66

--- q66/Main.cpp
+++ q66/Main.cpp
@@ -10,5 +10,24 @@ int main() {
         cin >> L[i] >> R[i];
     }
 
+    double answer = 0.0;
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            auto comb = 0;
+            auto cnt = 0;
+            for (int l = L[i]; l <= R[i]; l++) {
+                for (int k = L[j]; k <= R[j]; k++) {
+                    if (l > k) {
+                        cnt++;
+                    }
+                    comb++;
+                }
+            }
+
+            answer += double(cnt) / comb;
+        }
+    }
+
+    cout << fixed << setprecision(15) << answer << endl;
     return 0;
 }
