e9f4f602c1b12db8ec3c5a52753b3212b3be773d
adsholoko <ad.sho.loko@gmail.com>
2023-11-25 17:43:11 +0000

    Added explanatory image to the repository

--- q73/Main.cpp
+++ q73/Main.cpp
@@ -1,27 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<long long>> dp;
+vector<char> C;
+vector<vector<int>> G;
+
+void dfs(int now, int pre) {
+    for (auto next : G[now]) {
+        if (pre == next) {
+            continue;
+        }
+
+        dfs(next, now);
+
+        if (C[now] == 'a') {
+            dp[now][0] *= dp[next][0] + dp[next][2];
+        } else {
+            dp[now][1] *= dp[next][1] + dp[next][2];
+        }
+    }
+
+    if (C[now] == 'a') {
+        dp[now][0] = 1;
+        dp[now][2] = 0;
+        // dp[pos][2] = (val2 - val1 + mod) % mod;
+    }
+
+    if (C[now] == 'b') {
+        dp[now][1] = 1;
+        dp[now][2] = 0;
+        // dp[pos][2] = (val2 - val1 + mod) % mod;
+    }
+}
+
 int main() {
     int N;
     cin >> N;
 
-    vector<string> C(N);
-    vector<vector<int>> G(N, vector<int>());
+    C.resize(N + 1);
+    G.resize(N + 1, vector<int>());
+    dp.resize(N + 1, vector<long long>(3));
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         cin >> C[i];
     }
 
     for (int i = 0; i < N - 1; i++) {
         int a, b;
         cin >> a >> b;
-        a--;
-        b--;
         G[a].push_back(b);
         G[b].push_back(a);
-
-        cout << a << b << endl;
     }
 
+    dfs(1, -1);
     return 0;
 }
